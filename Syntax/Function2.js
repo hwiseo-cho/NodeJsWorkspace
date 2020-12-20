aa04afd7256dbfe2c7d784e68d6cd85d03d3492f
hwiseo-cho <gnltj37@naver.com>
2020-12-20 22:13:18 +0900

    hwiseo-cho

--- Syntax/Function2.js
+++ Syntax/Function2.js
@@ -2,6 +2,7 @@ console.log(Math.round(1.5));
 console.log(Math.round(1.4));
 console.log("--------------------");
 
+
 function sum(first, second) { // parameter 매개변수
     console.log(first + second);
 }
