1ffbb0649461d923591a4c8cb145c3c7f18714b2
hwiseo-cho <gnltj37@naver.com>
2020-12-21 23:22:45 +0900

    commit

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
     return `
     <!doctype html>
     <html>
     <head>
-    <title>WEB1 -${title}</title>
-    <meta charset="utf-8">
+        <title>WEB1 -${title}</title>
+        <meta charset="utf-8">
     </head>
     <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${body}
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        <a href="/create">create</a>
+        ${body}
     </body>
     </html>
     `;
@@ -32,11 +34,9 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    
     if(pathname === '/') {
         if(queryData.id === undefined) {
             fs.readdir('./data', function(error, fileList) {
-                console.log(fileList);
                 var title = "Welcome";
                 var description = "Hello, Node.js";
                 var list = templateList(fileList);
@@ -55,7 +55,41 @@ var app = http.createServer(function(request,response){
                 });
             });
         }
-    } else {
+    } else if(pathname === '/create') {
+        fs.readdir('./data', function(error, fileList) {
+            console.log(fileList);
+            var title = "WEB - create";
+            var list = templateList(fileList);
+            var template = templateHTML(title, list,
+                ` <form action="http://localhost:3000/create_process" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                  <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>`
+                );
+            response.writeHead(200);
+            response.end(template);
+        });
+    } else if(pathname === '/create_process') {
+        var body = "";
+        request.on('data',function(data) {
+            body += data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+                response.writeHead(302, {Location: `/?id=${title}`});
+                response.end('success!');
+            });
+        });
+        
+    }else {
         response.writeHead(404);
         response.end('Not Found');
     }
